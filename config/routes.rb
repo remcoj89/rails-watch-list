8ac685da3162d9985e312e3f54c9df00d1adb626
remcojonk <remcojonk@hotmail.com>
2022-11-17 21:29:44 +0100

    routes#bookmarkscontorler#new

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   root to: 'lists#index'
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :lists
-  resources :bookmarks
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
+
+# get: "/lists/1/bookmarks/new").to route_to(controller: "bookmarks", action: "new", list_id: "1")
