ff8d47ad58db782663de545cb96a940d30b4dc87
remcojonk <remcojonk@hotmail.com>
2022-11-19 13:11:50 +0100

    cloudinary#production

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,6 +2,5 @@ class Bookmark < ApplicationRecord
   belongs_to :list
   belongs_to :movie
   validates :comment, length: { minimum: 6 }
-  # validates :title, :movies, uniqueness: true
   validates :movie, uniqueness: { scope: :list }
 end
